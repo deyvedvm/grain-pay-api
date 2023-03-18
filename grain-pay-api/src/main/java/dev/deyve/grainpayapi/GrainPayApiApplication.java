463497c68231296280d21383e48c1c208b8980a6
Deyve Machado <deyvedvm@hotmail.com>
2023-03-18 14:08:31 -0300

    API - Inicial commit

---
+++ grain-pay-api/src/main/java/dev/deyve/grainpayapi/GrainPayApiApplication.java
@@ -0,0 +1,13 @@
+package dev.deyve.grainpayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrainPayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrainPayApiApplication.class, args);
+    }
+
+}
---
+++ grain-pay-api/src/test/java/dev/deyve/grainpayapi/GrainPayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.deyve.grainpayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrainPayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
